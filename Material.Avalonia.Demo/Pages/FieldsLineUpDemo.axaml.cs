2aa19d0c91a7721fe94021da7b512ca8619f67f2
Corvin <corvinszimion@googlemail.com>
2025-04-09 18:47:25 +0200

    .

--- Material.Avalonia.Demo/Pages/FieldsLineUpDemo.axaml.cs
+++ Material.Avalonia.Demo/Pages/FieldsLineUpDemo.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Material.Avalonia.Demo.ViewModels;
 
@@ -9,5 +10,31 @@ namespace Material.Avalonia.Demo.Pages;
 public partial class FieldsLineUpDemo : UserControl {
     public FieldsLineUpDemo() {
         InitializeComponent();
+    }
+
+    private void HasClearButton_IsCheckedChanged(object? sender, RoutedEventArgs e) {
+        bool shouldAddClearBtn = cbHasClearButton.IsChecked ?? false;
+
+        foreach (var child in FieldsLineUpGrid.Children) {
+
+            if (child.GetType() == typeof(TextBlock)) {
+                continue;
+            }
+
+            if (child is not StyledElement styleable) {
+                continue;
+            }
+
+            if (shouldAddClearBtn) {
+                if (!styleable.Classes.Contains("clearButton")) {
+                    styleable.Classes.Add("clearButton");
+                }
+            }
+            else {
+                if (styleable.Classes.Contains("clearButton")) {
+                    styleable.Classes.Remove("clearButton");
+                }
+            }
+        }
     }
 }
